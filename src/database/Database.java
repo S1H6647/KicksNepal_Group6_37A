2157707b072d6deac4c27db3634005840cdf6db1
Sanjit Hyanju <sanjithyanju99@gmail.com>
2025-05-21 20:23:07 +0545

    Addition: Database connectivity

--- src/database/Database.java
+++ src/database/Database.java
@@ -4,10 +4,16 @@
  */
 package database;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 /**
  *
  * @author sanji
  */
-public class Database {
-    
+public interface Database {
+    Connection openConnection();
+    void closeConnection(Connection connection);
+    ResultSet runQuery(Connection connection, String query);
+    int executeUpdate(Connection connection, String query);
 }
--- src/database/MySqlConnection.java
+++ src/database/MySqlConnection.java
@@ -4,10 +4,72 @@
  */
 package database;
 
+import java.sql.*;
+
 /**
  *
  * @author sanji
  */
-public class MySqlConnection {
-    
+public class MySqlConnection implements Database{
+    @Override
+    public Connection openConnection() {
+        try {
+            String username = "root";
+            String password = "s1h3347";
+            String database = "kicksnepal";
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/" + database,username,password
+            );
+            if (connection == null){
+                System.out.println("Database connection fail!");
+            }
+            else {
+                System.out.println("Database connection success!");
+            }
+            return connection;
+        }
+        catch (SQLException e){
+            System.out.println("Connection error: "+ e);
+            return null;
+        }
+    }
+
+    @Override
+    public void closeConnection(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()){
+                conn.close();
+                System.out.println("Connection closed");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("Connection error: "+ e);
+        }
+    }
+
+    @Override
+    public ResultSet runQuery(Connection conn, String query) {
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(query);
+            return result;
+        }
+        catch (SQLException e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    @Override
+    public int executeUpdate(Connection conn, String query) {
+        try {
+            Statement statement = conn.createStatement();
+            int result = statement.executeUpdate(query);
+            return result;
+        }
+        catch (SQLException e){
+            System.out.println(e);
+            return -1;
+        }
+    }
 }
